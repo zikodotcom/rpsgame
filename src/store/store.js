01ceb151dbea0f8808bb91768c6830729fd32838
zikodotcom <foot.ball.american123@gmail.com>
2024-07-07 18:01:04 +0100

    Add vercel link

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,7 @@ export const store = createStore({
   state() {
     return {
       backendLink: 'http://192.168.1.103:3000/',
-      frontLink: 'http://192.168.1.103:5173'
+      frontLink: 'https://rpsgame-gules.vercel.app/'
     }
   }
 })
